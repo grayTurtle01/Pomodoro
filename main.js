e03edb17e0d406ea7027945e008788370dbdfcd4
grayTurtle01 <the.correo@protonmail.com>
2022-01-14 15:46:41 -0600

    vanilla-js working

--- main.js
+++ main.js
@@ -6,15 +6,33 @@ let state = "session"
 
 let minutes = session_time;
 let seconds = 0;
+let max_seconds = 59
 let str_timer = '25:00'
 
 let audio = document.querySelector("audio")
+
+// dev-variables
+//~ break_time = 1
+//~ session_time = 1
+//~ minutes = session_time
+//~ str_timer = '01:00'
+//~ max_seconds = 10
+
+function init(){
+    document.querySelector("#break-length").innerText = break_time
+    document.querySelector("#session-length").innerText = session_time
+    document.querySelector("#time-left").innerText = str_timer
+}
+init()
+
 /// reset
 document.querySelector("#reset").onclick = function(){
 
-    //stop timer
+    //stop beep
     audio.pause()
     audio.load()
+    
+    //stop timer
     is_playing = false;
     start_stop()
    
@@ -25,7 +43,6 @@ document.querySelector("#reset").onclick = function(){
     state = "session"
     
     
-
     //break to 5m
     break_time = 5
     document.querySelector("#break-length").innerText = break_time
@@ -44,24 +61,26 @@ document.querySelector("#reset").onclick = function(){
 
 /// break decrement
 document.querySelector("#break-decrement").onclick = function(){
-    if( break_time > 1){
+    if( (break_time > 1) && !is_playing){
         break_time -= 1;
         document.querySelector("#break-length").innerText = break_time
+        update_timmer()
     }
 }
 
 /// break increment
 document.querySelector("#break-increment").onclick = function(){
-    if( break_time < 60 ){
+    if( break_time < 60 && !is_playing ){
         break_time += 1;
         document.querySelector("#break-length").innerText = break_time
+        update_timmer()
     }
 }
 
 
 /// session decrement
 document.querySelector("#session-decrement").onclick = function(){
-    if( session_time > 1){
+    if( session_time > 1 && !is_playing){
         session_time -= 1;
         document.querySelector("#session-length").innerText = session_time
         minutes--;
@@ -72,7 +91,7 @@ document.querySelector("#session-decrement").onclick = function(){
 
 /// session increment
 document.querySelector("#session-increment").onclick = function(){
-    if( session_time < 60){
+    if( session_time < 60 && !is_playing){
         session_time += 1;
         document.querySelector("#session-length").innerText = session_time
         minutes++
@@ -81,38 +100,29 @@ document.querySelector("#session-increment").onclick = function(){
     
 }
 
+
 /// stop-play
 document.querySelector("#start_stop").onclick = function(){
     is_playing = !is_playing
     start_stop()
-    document.getElementById("myAudio")
 }
-
 
 
 
 function start_stop(){
 
     if( is_playing ){
-        interval = setInterval(decrement_seconds, 1000)
+        interval = setInterval(decrement_second, 1000)
     }
     else{
         try{
             clearInterval(interval)
-            audio.pause()
-            aduio.load()
         }catch{}
     }
 }
 
-let max_seconds = 59
-max_seconds = 5
-minutes = 1
 
-session_time = 1
-break_time = 1
-
-function decrement_seconds(){
+function decrement_second(){
     if( seconds == 0){
         minutes--;
         seconds = max_seconds
@@ -121,14 +131,13 @@ function decrement_seconds(){
     else if( seconds > 0){
         seconds--;        
     }
-    else{
-        seconds = 0
-    }
-    
+        
     update_timmer()
+    check_state()
 }
 
 function update_timmer(){
+    
     formatted_seconds = seconds.toLocaleString('en-US', {
                                 minimumIntegerDigits: 2,
                                 useGrouping: false
@@ -142,29 +151,46 @@ function update_timmer(){
     str_timer = formatted_minutes + ':' + formatted_seconds
     document.querySelector("#time-left").innerText = str_timer
 
-    check_state()
+    //check_state()
+    
 }
 
 
-function check_state(){
+async function check_state(){
+    
      if( state == "session")
         if( minutes == 0 && seconds == 0){
             audio.play()
+            is_playing = false
+            start_stop()
             state = "break"
-            document.querySelector("#timer-label").innerText = 'Break'
-            //~ minutes = break_time;
             minutes = document.querySelector("#break-length").innerText;
             seconds = 0;
+            
+            await setTimeout( ()=> {
+               is_playing = true
+               start_stop() 
+               document.querySelector("#timer-label").innerText = 'Break'
+               update_timmer()
+            } , 3000)
         }
         
     if( state == "break")
         if( minutes == 0 && seconds == 0){
             audio.play()
+            is_playing = false
+            start_stop()
             state = "session"
-            document.querySelector("#timer-label").innerText = 'Session'
-            //~ minutes = session_time;
             minutes = document.querySelector("#session-length").innerText;
             seconds = 0;
+
+           await setTimeout( ()=> {
+               is_playing = true
+               start_stop() 
+               document.querySelector("#timer-label").innerText = 'Session'
+               update_timmer()
+            } , 3000)
+            
         }
 
 }
